fd871bf8e25d9a95b18015f36956575ca967272a
dylansarikas <dylan.sarikas@gmail.com>
2024-06-26 13:09:31 -0500

    adds director create to controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,5 +6,20 @@ class DirectorsController < ApplicationController
   def index
     @directors = Director.order(created_at: :desc)
   end
+
+  def create
+    director_attributes = params.require(:director).permit(:name, :dob, :bio, :films)
+    @director = Director.new(director_params)
+
+    if @director.valid?
+      @director.save
+      redirect_to directors_url, notice: "Director created successfully"
+    else
+      render "new"
+    end
+  end
   
+  def director_params
+    params.require(:director).permit(:name, :dob, :bio, :films)
+  end
 end
