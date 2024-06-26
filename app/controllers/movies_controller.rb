0b89add940525ba3d0794324046e1cb2289166cc
dylansarikas <dylan.sarikas@gmail.com>
2024-06-26 11:46:59 -0500

    refactors movie create in controller to protect against attacks

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,9 +20,8 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params[:movie][:title]
-    @movie.description = params[:movie][:description]
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
